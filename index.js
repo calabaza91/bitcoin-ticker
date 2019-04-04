b485d83e6d120989bf2908fa122d9efda18e205d
Caleb Cox <calebcox2010@gmail.com>
2019-04-04 13:00:04 -0500

    Update index.js with request module and post function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
@@ -10,7 +11,20 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res){
+  // console.log(req.body.crypto);
+  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(err, res, body){
 
+    //Select all data from API with parse
+    var data = JSON.parse(body);
+    //Select most recent price
+    var price = data.last;
+
+    console.log(price);
+
+  });
+
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
